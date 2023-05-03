809560f15b48de7033016fa6f840f5d2ed54d2ff
Admin <Admin@DESKTOP-BP9VF06>
2023-05-03 14:39:42 +0800

    awfserhg

---
+++ Immutable/src/Course.java
@@ -0,0 +1,19 @@
+
+public class Course {
+
+private String courseName;
+	
+	Course(String courseName) {
+		this.courseName = courseName;
+	}
+	
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String  courseName) {
+		this.courseName = courseName;
+
+	}
+
+}
